f5edba597dadc92f262e02a246f9f8b0249ede5e
afrzalbudi1 <32351812+afrizalbudi1@users.noreply.github.com>
2018-01-25 17:34:46 +0800

    Membuat Int Mail Nama, Ukuran, Berat dewasa

--- Elang.cpp
+++ Elang.cpp
@@ -8,4 +8,19 @@ class elang{
 		double beratdewasa;
 		string ukuran;
 		string nama;	
+};
+
+
+int main(){
+	elang ElangHutan,ElangAlaska,ElangSteppe;
+	
+	ElangHutan.nama = "Elang Hutan";
+	ElangHutan.ukuran = "SEDANG";
+	ElangHutan.beratdewasa = 40.5;
+	ElangAlaska.nama = "Elang Alaska";
+	ElangAlaska.ukuran = "BESAR";
+	ElangAlaska.beratdewasa = 65.1;
+	ElangSteppe.nama = "Elang Steppe";
+	ElangSteppe.ukuran = "Besar";
+	ElangSteppe.beratdewasa = 61.7;
 }
